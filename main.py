fc9a0f7bbe99f261109de7572cf29a9bf162c07b
n-leezy <loahnisk@gmail.com>
2025-01-31 03:46:02 -0600

    created groups to be used for game sprites

--- main.py
+++ main.py
@@ -12,22 +12,29 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    clock = pygame.time.Clock()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # sets the screen to be used
+    clock = pygame.time.Clock() # instantiates the clock variable
 
-    x = SCREEN_WIDTH / 2
+    x = SCREEN_WIDTH / 2 # finds the midpoint of the screen
     y = SCREEN_HEIGHT / 2
-    player = Player(x, y)
+    player = Player(x, y) # instantiate player class
+
+    updateables = pygame.sprite.Group(player) # group the updateable sprites together
+    drawables = pygame.sprite.Group(player) # group the drawable sprites together
 
     dt = 0
+
+    # beginning of the game loop
     while True:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT: # quits the program if the exit button is pressed
                 pygame.quit()
                 return
-        pygame.Surface.fill(screen, BLACK)
-        player.draw(screen)
-        player.update(dt)
+        pygame.Surface.fill(screen, BLACK) # fills the game screen with black
+        for updateable in updateables: # updates the updateables
+            updateable.update(dt)
+        for drawable in drawables: # draws the drawables
+            drawable.draw(screen)
         pygame.display.flip()
         delta_time = clock.tick(60) # Pauses the game loop until 1/nth of a second has passed, returns the amount of time passed since last called
         dt = delta_time / 1000 # stores the delta time in seconds into dt
